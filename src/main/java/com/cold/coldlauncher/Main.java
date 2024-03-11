87eb21587871ac42be18c5a9a5458ee36f735e5c
cold233 <1057538754@qq.com>
2024-03-11 14:48:31 +0000

    try adding validation method

--- src/main/java/com/cold/coldlauncher/Main.java
+++ src/main/java/com/cold/coldlauncher/Main.java
@@ -4,6 +4,11 @@ import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) {
-        Long.parseLong("4c34ae96f84641cc",16);
+        BigInteger highBitUUID = new BigInteger("4c34ae96f84641cca76500a0c91e6bf6",16);
+        System.out.printf("%h\n",highBitUUID);
+        BigInteger lowBitUUID = new BigInteger("a76500a0c91e6bf6",16);
+        System.out.printf("%h\n",lowBitUUID);
+        //BigInteger uuid=highBitUUID.shiftLeft(32).add(lowBitUUID);
+        //System.out.printf("%h\n",uuid);
     }
 }
--- src/main/java/com/cold/coldlauncher/infrastructure/Player.java
+++ src/main/java/com/cold/coldlauncher/infrastructure/Player.java
@@ -1,9 +1,46 @@
 package com.cold.coldlauncher.infrastructure;
 
-import java.math.BigInteger;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Player {
     private Icon icon;
     private String name;
-    private BigInteger uuid;
+    private String uuid;
+    public Player(String nameIn){
+        this.name=nameIn;
+    }
+
+    public Icon getIcon() {
+        return icon;
+    }
+
+    public void setIcon(Icon icon) {
+        this.icon = icon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean setName(String name) {
+        this.name = name;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public boolean setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+    public boolean validateName(String name){
+        String pattern="[a-z]";
+        Pattern r = Pattern.compile(pattern);
+        if(name.length()<3|name.length()>16) return false;
+        else {
+            Matcher matcher = r.matcher()
+        }
+    }
 }
